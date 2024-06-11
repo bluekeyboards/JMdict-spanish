1ac54bd7d5c483d568cb7d7a3cb166fa106255b8
bluekeyboards <139714727+bluekeyboards@users.noreply.github.com>
2024-06-12 00:30:00 +0200

    second commit

--- pot-maker.bat
+++ pot-maker.bat
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 python step1.py
 xgettext -d JMdict_es --its="rules.its" --no-wrap "JMdict_e.xml"
+=======
+pushd "C:\Users\anny_\Documents\JMDict spanish"
+python step1.py
+xgettext -d JMdict_es --its="rules.its" --no-wrap "JMdict_e.xml"
+>>>>>>> 847af32 (initial commit)
 python step2.py
--- step2.py
+++ step2.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # make msgid multiline
 
 import re
@@ -15,4 +16,23 @@ filedata = re.sub(sub2, 'n', filedata, flags=0)
 with open ('JMdict_es.po', 'w', encoding='utf-8') as file:
     file.write(filedata)
     
+=======
+# make msgid multiline
+
+import re
+with open('JMdict_es.po', encoding='utf-8') as file:
+    filedata = file.read()
+    
+sub1 = '\\\\n '
+sub2 = '\\\\n'
+
+filedata = re.sub(sub1, '\\\\n"\n"', filedata, flags=0)
+filedata = re.sub(sub2, 'n', filedata, flags=0)
+
+#print(filedata)
+
+with open ('JMdict_es.po', 'w', encoding='utf-8') as file:
+    file.write(filedata)
+    
+>>>>>>> 847af32 (initial commit)
 file.close()

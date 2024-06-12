0f351e421c0a3dbe6613d29719376cfaae08b232
bluekeyboards <139714727+bluekeyboards@users.noreply.github.com>
2024-06-12 11:52:05 +0200

    desktop commit 3

--- pot-maker.bat
+++ pot-maker.bat
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 python step1.py
 xgettext -d JMdict_es --its="rules.its" --no-wrap "JMdict_e.xml"
-=======
-pushd "C:\Users\anny_\Documents\JMDict spanish"
-=======
->>>>>>> 52e98ac (initial commit)
-python step1.py
-xgettext -d JMdict_es --its="rules.its" --no-wrap "JMdict_e.xml"
->>>>>>> 847af32 (initial commit)
 python step2.py
